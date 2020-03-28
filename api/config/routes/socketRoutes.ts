d59cb55eb796c65418cc489294692bc4f4d4a00b
George <george.ridgway@protonmail.com>
2020-03-28 16:35:09 +0000

    trs# Please enter the commit message for your changes. Lines starting

--- api/config/routes/socketRoutes.ts
+++ api/config/routes/socketRoutes.ts
@@ -1,19 +1,20 @@
-import { controller as socketController } from "../../controllers/SocketController";
+import { controller as socketController } from '../../controllers/SocketController'
 
 module.exports = (io: SocketIO.Server) => {
-  io.on("connection", socket => {
-    console.log("user has connected!");
-    socket.on("startGoogleCloudStream", request => {
-      socketController.startStream(socket, request);
-    });
+  io.on('connection', socket => {
+    console.log('user has connected!')
+    socket.on('startGoogleCloudStream', request => {
+      socketController.startStream(socket, request)
+      console.log('hkhjhkjh')
+    })
     // Receive audio data
-    socket.on("binaryAudioData", data => {
-      socketController.onAudioDataReceived(data);
-    });
+    socket.on('binaryAudioData', data => {
+      socketController.onAudioDataReceived(data)
+    })
 
     // End the audio stream
-    socket.on("endGoogleCloudStream", () => {
-      socketController.endStream();
-    });
-  });
-};
+    socket.on('endGoogleCloudStream', () => {
+      socketController.endStream()
+    })
+  })
+}
